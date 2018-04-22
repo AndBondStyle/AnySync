7833e12a67d2f845577ee9f5405fcf7e8c6149ee
AndBondStyle <andbondstyle@gmail.com>
2018-04-22 22:39:41 +0300

    Nonexistent table fix

--- backend/streams/__init__.py
+++ backend/streams/__init__.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db import connection
 
 
 class Config(AppConfig):
@@ -6,6 +7,8 @@ class Config(AppConfig):
 
     def ready(self):
         from .models import Stream
+        tables = connection.introspection.table_names()
+        if 'streams_stream' not in tables: return
         Stream.objects.all().update(online=0)
 
 default_app_config = 'backend.streams.Config'
