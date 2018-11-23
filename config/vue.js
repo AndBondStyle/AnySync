ee4d78221a265922da76a8989a804f75fd63a83a
AndBondStyle <andbondstyle@gmail.com>
2018-11-23 10:55:01 +0300

    New initial commit

---
+++ config/vue.js
@@ -0,0 +1,16 @@
+module.exports = {
+  outputDir: 'public',
+  baseUrl: undefined,
+  assetsDir: 'static',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
+  },
+
+  pwa: {
+    name: 'AnySync'
+  }
+}
