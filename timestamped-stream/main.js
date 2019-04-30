3202a4dbe0539c665a36b9663998960098b3651e
AndBondStyle <andbondstyle@gmail.com>
2019-04-30 23:56:43 +0300

    Timestamped stream update

--- timestamped-stream/main.js
+++ timestamped-stream/main.js
@@ -1,14 +1,13 @@
 import 'babel-polyfill';
-import { AudioContext } from 'standardized-audio-context';
 import axios from 'axios';
 import Peer from 'peerjs';
 
 const URL = 'https://archive.org/download/testmp3testfile/mpthreetest.mp3';
 const CONFIG = {key: 'lwjd5qra8257b9'}; // PeerJS public server key
-const BUFFERING = 2000; // Single chunk length (ms)
+const BUFFERING = 5000; // Single chunk length (ms)
 const DELAY = 1000; // Emulated stream delay (ms)
 
-let time = () => new Date().getTime(); // Current time shortcut
+let time = () => context.currentTime * 1000; // Current time shortcut
 let btn = document.querySelector('#btn'); // Button element
 btn.onclick = init;
 
@@ -21,7 +20,6 @@ let currtime = null; // Current chunk timestamp ({DELAY} ms ahead from actual ti
 let promise = null; // Promise holder to avoid callback madness
 let resolve = null; // Promise resolve callback (used with decoder)
 let firstchunk = null; // First chunk retrieved from recorder (contains OPUS header)
-let decoder = null; // Custom OPUS decoder (chunked)
 
 async function init() {
     btn.onclick = () => null;
@@ -30,12 +28,12 @@ async function init() {
     console.log('Setting up peer connection A...');
     a = new Peer(CONFIG);
     promise = new Promise(r => a.on('open', r));
-    await promise
+    await promise;
 
     console.log('Setting up peer connection B...');
     b = new Peer(CONFIG);
     promise = new Promise(r => b.on('open', r));
-    await promise
+    await promise;
 
     console.log('Establishing A <-> B connection...');
     promise = new Promise(r => b.on('connection', r));
@@ -47,24 +45,16 @@ async function init() {
     context = new AudioContext();
     let stream = await initStream();
 
-    function onDecode({left, right, samplesDecoded, sampleRate}) {
-        console.log('DECODED:', left, right, samplesDecoded);
-    }
-
-    console.log('Setting up decoder (nope)...');
-    // decoder = new OpusStreamDecoder({onDecode: v => {
-    //     console.log('DECODED:', v);
-    //     resolve(v);
-    // }});
-    // decoder = new OpusStreamDecoder({onDecode});
-    // await decoder.ready;
+    // console.log('Setting up decoder...');
+    // decoder = new Decoder({channels: 2, fallback: false});
+    // decoder.decoder.flushLimit = 1;
 
     console.log('Setting up recorder...');
     let recorder = new MediaRecorder(stream);
     recorder.addEventListener('dataavailable', sendChunk);
     recorder.start(BUFFERING);
 
-    console.log('Waiting for the first chunk...')
+    console.log('Waiting for the first chunk...');
     btn.innerText = 'BUFFERING...';
     promise = new Promise(r => setTimeout(r, BUFFERING));
     await promise;
@@ -93,7 +83,7 @@ async function initStream() {
 }
 
 async function sendChunk(event) {
-    if (firstchunk == null) firstchunk = event.data;
+    if (firstchunk == null) firstchunk = await new Response(event.data).arrayBuffer();
     if (currtime == null) currtime = time() + DELAY;
     if (prevtime == null) prevtime = event.timecode;
     let delta = event.timecode - prevtime;
@@ -105,28 +95,34 @@ async function sendChunk(event) {
 }
 
 async function recieveChunk(data) {
-    if (data.first) {
-        promise = new Promise(r => resolve = r);
-        let arr = new Uint8Array(data.blob);
-        console.log(data, arr);
-        // decoder.decode(arr);
-        await promise; return;
-    }
-
-    // let buffer = await decode(data.blob);
+    if (data.first) return;
+    let buffer = await decode(data.blob);
     let source = context.createBufferSource();
-    let start = context.currentTime + (data.timestamp - time()) / 1000;
+    let start = context.currentTime +(data.timestamp - time()) / 1000;
     console.log('NOW:', context.currentTime, 'PLAY AT:', start);
-    // source.buffer = buffer;
+    source.buffer = buffer;
     source.connect(context.destination);
     source.start(start);
     console.log('[B] Scheduled chunk with timestamp:', data.timestamp);
 }
 
 async function decode(blob) {
-    await promise;
-    promise = new Promise(r => resolve = r);
-    decoder.decode(new Uint8Array(blob));
-    let decoded = await promise;
-    debugger;
+    let headersize = 162;
+    let header = firstchunk.slice(0, headersize);
+    console.log('HEADER LAST BYTE:', (new Uint8Array(header))[header.byteLength - 1].toString(16));
+    console.log('CHUNK FIRST BYTE:', (new Uint8Array(blob, 0, 1))[0].toString(16));
+    let data = new Uint8Array(header.byteLength + blob.byteLength);
+    data.set(new Uint8Array(header), 0);
+    data.set(new Uint8Array(blob), header.byteLength);
+    return context.decodeAudioData(data.buffer);
+}
+
+function download(data) {
+    if (data == null) return;
+    let link = document.createElement('a');
+    let blob = new Blob([data]);
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'audio.opus';
+    link.click();
+    link.remove();
 }
